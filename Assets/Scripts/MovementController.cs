9a76c612cca6cca2c923ea1a5612129077c9b0e8
Nehemias Munoz <nehemiasmunozm@gmail.com>
2022-11-07 14:32:42 -0300

    Salto personaje

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,15 +7,44 @@ public class MovementController : MonoBehaviour
     // clase con metodos predifinidos para el movimiento, collisiones sin rigibody   
     public CharacterController controller;
     [SerializeField] private float speed = 20f;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private Transform groundCheck;
+    private bool isGrounded;
+    private float groundDistance = 0.3f;
+    private Vector3 movement;
+    private Vector3 velocity;
+    [SerializeField] private LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
         float xMove = Input.GetAxis("Horizontal");
         float zMove = Input.GetAxis("Vertical");
         
         //mover elemento en el eje x y z
-        Vector3 movement = transform.right * xMove + transform.forward * zMove;
+        movement = transform.right * xMove + transform.forward * zMove;
         controller.Move(movement * (speed * Time.deltaTime));
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        
     }
 }
+
+
