7e3dba28d2ff67c10e044b1aa95b77ce39614d2e
Nehemias Munoz <nehemiasmunozm@gmail.com>
2022-11-07 16:58:59 -0300

    Generacion de barco y barriles

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,19 +7,24 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private GameObject barrelModel;
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject shipModel;
 
-    private int numberOfBarrels = 10;
+    private int numberOfBarrels = 50;
     // Start is called before the first frame update
     void Start()
     {
-        player.transform.position = new Vector3(0, 2, 0);
         GenerateLevel();
+        player.transform.position = new Vector3(0, 3, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Todo: fix
+        // if (player.transform.position.y <= -0.4f)
+        // {
+        //     player.transform.position = new Vector3(0, 10, 0);
+        // }
     }
 
 
@@ -28,8 +33,13 @@ public class GameManager : MonoBehaviour
         Instantiate(barrelModel, new Vector3(0, 1, 0), Quaternion.identity);
         for (int i = 0; i < numberOfBarrels; i++)
         {
-            Instantiate(barrelModel, new Vector3(Random.Range(1,4),Random.Range(1,2),i), Quaternion.identity);
-           
+            Instantiate(barrelModel, new Vector3(Random.Range(-4,4),1,i*2.0F), Quaternion.Euler(0,Random.Range(-10,10), 0));
+
+            if (i == numberOfBarrels-1)
+            {
+                Instantiate(shipModel, new Vector3(0, 2, i *2.3f), Quaternion.Euler(0,90,0));
+            }
         }
+        
     }
 }
