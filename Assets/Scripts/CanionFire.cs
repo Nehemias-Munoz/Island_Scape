d345bdfc0c512bedd79ed1abfa3412f5c7024ad8
Nehemias Munoz <nehemiasmunozm@gmail.com>
2022-11-09 16:36:56 -0300

    Espacion entre disparo y sonido de canion

--- Assets/Scripts/CanionFire.cs
+++ Assets/Scripts/CanionFire.cs
@@ -3,11 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class CanionFire : MonoBehaviour
 {
     private bool isGunLoaded = true;
     [SerializeField] private GameObject bulletModel;
+
+    [Header("Audio stuff")] 
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip shootSound;
+    
     [FormerlySerializedAs("Aim")] [SerializeField] private Transform aim;
     private Transform player;
     private string playerTag = "Player";
@@ -34,6 +40,7 @@ public class CanionFire : MonoBehaviour
     void Shoot()
     {
         GameObject newBullet;
+        PlaySound();
         newBullet = Instantiate(bulletModel, aim.position, Quaternion.identity);
         newBullet.GetComponent<Rigidbody>().AddForce(aim.forward * shootForce);
         Destroy(newBullet,2);
@@ -41,9 +48,13 @@ public class CanionFire : MonoBehaviour
     }
     IEnumerator ReloadCanion()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(Random.Range(2.5f,3.5f));
         isGunLoaded = true;
     }
 
+    void PlaySound()
+    {
+        _audioSource.PlayOneShot(shootSound);
+    }
    
 }
