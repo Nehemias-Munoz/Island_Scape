63ab64f05eb53ede16b402450cc8e7bd0aa78084
Nehemias Munoz <nehemiasmunozm@gmail.com>
2022-11-17 20:15:52 -0300

    Borrando llamadas inecesarias

--- Assets/Scripts/FollowDestination.cs
+++ Assets/Scripts/FollowDestination.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowDestination : MonoBehaviour
 {
-  [SerializeField] GameObject[] destinationPoints;
-  [SerializeField] Transform destination;
+  GameObject[] destinationPoints;
+  Transform destination;
   public float speed = 5.0f; 
     // Start is called before the first frame update
     void Start()
