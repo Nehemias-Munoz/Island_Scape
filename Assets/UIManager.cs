fee12b567b936f87a9f98ae59173970c7a8c34a1
Nehemias Munoz <nehemiasmunozm@gmail.com>
2022-11-26 17:23:56 -0300

    Nuevo Menu, correccion altura mar

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,25 +5,57 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private string gamePlayScene;
+    [SerializeField] private GameObject pausePanel;
+    public static UIManager instance;
+
+    public bool isGamePause = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowPauseMenu(true);
+        Time.timeScale = 0;
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isGamePause = !isGamePause;
+            Pause();
+        }
+        if(Input.GetKeyDown(KeyCode.E) && isGamePause)
+        {
+            print("Salir");
+        }
+
     }
 
-    void Play()
+    public void Pause()
     {
-        SceneManager.LoadScene(gamePlayScene);
-        Time.timeScale = 1;
-       // score = 0;
-       //  difficulty = 1;
-       //  gameOver = false;
+        if (isGamePause)
+        {
+            Time.timeScale = 0;
+            ShowPauseMenu(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            ShowPauseMenu(false);
+        }
     }
+
+    void ShowPauseMenu(bool isActive)
+    {
+        pausePanel.SetActive(isActive);
+    }
+
+    //private void Exit()
+    //{
+
+    //}
+
 }
